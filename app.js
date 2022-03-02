a07a418b46ac850864a1a62fbd30808640bc35d1
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-03-02 22:37:35 +0500

    added change

--- app.js
+++ app.js
@@ -1,67 +1,10 @@
 const api = "https://covid19.mathdro.id/api/confirmed";
 const list = document.querySelector(".list");
 const input = document.querySelector("#form_input");
-let fullData = [];
-let filteredData = [];
+  
 
-useFetch(api);
-async function useFetch(url) {
-  try {
-    const req = await fetch(url);
-    if (!req.ok) {
-      throw new Error("xatolik bor");
-    }
-    const data = await req.json();
-    fullData = data;
-    $("#form").css("display", "block");
-    
-  } catch (err) {
-    console.log(err.message);
-  }
-}
 
-function showData(data) {
-  const allCountry = data;
-  allCountry.forEach((country) => {
-
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <h1 style="margin-right:30px;">Mamlakat nomi:${country.countryRegion}</h1>
-    <p style="margin-right:30px;">Kasallanganlar:${country.confirmed}</p>
-    <p style="margin-right:30px;">Vafot etganlar:${country.deaths}</p>
-
-    `;
-
-    
-    
-    $(".list").append(div);
-
-  });
-}
-
-// $("#form").submit(function (e) {
-//   e.preventDefault();
-//   const a = $("#form_input").val();
-//   filteredData = [];
-//   $('.list').empty()
-//   fullData.forEach((country) => {
-//     if (country.name.toLowerCase().includes(a.toLowerCase())) {
-//       filteredData.push(country);
-//     }
-//   });
-//   showData(filteredData);
-// });
-
-input.addEventListener('input',(e)=>{
-    e.preventDefault();
-    const a = $("#form_input").val();
-    filteredData = [];
-    $(".list").empty();
-    fullData.forEach((country) => {
-      if (country.countryRegion.toLowerCase().includes(a.toLowerCase())) {
-        filteredData.push(country);
-      }
-    });
-    showData(filteredData);
-
+const api = "https://covid19.mathdro.id/api/countries/";
+$('#form').submit((e)=>{
+  e.pre  
 })
